1e3ee0a9ab11a2c959dea202128ac6339e8b2693
jyotishka7139 <jyotishka.dasgupta@byjus.com>
2022-12-09 03:54:47 +0530

    parser for all tests pass

--- index.js
+++ index.js
@@ -60,85 +60,85 @@ try {
 
   //function for parse every XML:
   function parseData(inData) {
-    let FULLSTR = "";
+    let testReport = "";
 
-    inData.forEach(function (fileName) {
-      // per file actions here, include only TEST files:
-      if (fileName.includes("test")) {
-        //grab file and populate content to JS object:
-        var convert = require("xml-js");
-        //TEST ONLY:
-        //var xml = require('fs').readFileSync(tempdirnameString + '/' + fileName, 'utf8');
-        //PROD ONLY:
-        var xml = require("fs").readFileSync(testDir + "/" + fileName, "utf8");
-        var options = { ignoreComment: true, alwaysChildren: true };
-        var content = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
-        //process and output attributes:
-        //if (short output stirng for passed test) else (long output string for failed test):
-        console.log("content: ", content);
-        console.log(
-          util.inspect(content, false, null, true /* enable colors */)
-        );
-        console.log("content attributes: ", content.elements[0].attributes);
-        console.log("content elements FAIL: ", content.elements[0].elements);
+    // per file actions here, include only TEST files:
+    // if (fileName.includes("test")) {
+    //grab file and populate content to JS object:
+    var convert = require("xml-js");
+    //TEST ONLY:
+    //var xml = require('fs').readFileSync(tempdirnameString + '/' + fileName, 'utf8');
+    //PROD ONLY:
+    var xml = require("fs").readFileSync(testDir + "/" + fileName, "utf8");
+    var options = { ignoreComment: true, alwaysChildren: true };
+    var content = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
+    //process and output attributes:
+    //if (short output stirng for passed test) else (long output string for failed test):
+    console.log("content: ", content);
+    console.log(util.inspect(content, false, null, true /* enable colors */));
+    console.log("content attributes: ", content.elements[0].attributes);
+    console.log("content elements FAIL: ", content.elements[0].elements);
 
-        console.log("calculating test results, deciding if abbreviate output");
-        if (
-          parseInt(content.elements[0].attributes.skipped) == 0 &&
-          parseInt(content.elements[0].attributes.failures) == 0 &&
-          parseInt(content.elements[0].attributes.errors) == 0
-        ) {
-          console.log(
-            "building abbreviated string for " +
-              content.elements[0].attributes.name
-          );
-          let SHORTOUTPUT =
-            "" + content.elements[0].attributes.name + " ALL PASSED:";
-          for (
-            let i = 0;
-            i < parseInt(content.elements[0].attributes.tests);
-            i++
-          ) {
-            SHORTOUTPUT += " :green_apple:";
+    console.log("calculating test results, deciding if abbreviate output");
+    if (
+      parseInt(content.elements[0].attributes.failures) == 0 &&
+      parseInt(content.elements[0].attributes.errors) == 0
+    ) {
+      console.log(
+        "building abbreviated string for " + content.elements[0].attributes.name
+      );
+      let reportContent =
+        "" + content.elements[0].attributes.name + " ALL TESTS PASSED:";
+      reportContent += "\r\n\r\n";
+      content.elements[0].elements.forEach((testSuite) => {
+        reportContent +=
+          ":white_check_mark: " + testSuite.attributes.name + "\r\n\r\n";
+        let testDescription = null;
+        testSuite.elements.forEach((test) => {
+          if (testDescription != test.attributes.classname) {
+            testDescription = test.attributes.classname;
+            reportContent += test.attributes.classname + "\r\n";
           }
-          SHORTOUTPUT += "\r\n";
-          FULLSTR += SHORTOUTPUT;
-        } else {
-          console.log(
-            "building long string for failed test: " +
-              content.elements[0].attributes.name
-          );
-          let OUTPUTSTR = "";
-          OUTPUTSTR += content.elements[0].attributes.name;
-          OUTPUTSTR += " HAS ERRORS:";
-          OUTPUTSTR += "\r\n";
+          reportContent +=
+            " :white_check_mark: " + test.attributes.name + "\r\n";
+        });
+      });
+      reportContent += "\r\n";
+      testReport += reportContent;
+    } else {
+      console.log(
+        "building long string for failed test: " +
+          content.elements[0].attributes.name
+      );
+      let OUTPUTSTR = "";
+      OUTPUTSTR += content.elements[0].attributes.name;
+      OUTPUTSTR += " HAS ERRORS:";
+      OUTPUTSTR += "\r\n";
 
-          //begin test case details
-          const testcases = content.elements[0].elements;
-          testcases.forEach(function (item) {
-            if (item.name == "testcase") {
-              var testCaseNameResult = item.attributes.name;
-              testCaseNameResult += getTestCaseResult(item);
-              if (testCaseNameResult.includes("skipped")) {
-                testCaseNameResult = ":pineapple: " + testCaseNameResult;
-              } else if (testCaseNameResult.includes("failure")) {
-                var errorMessage = item.elements[0].attributes.message;
-                var shortMessage = errorMessage.split(/\r?\n/)[0];
-                testCaseNameResult =
-                  ":apple: " + testCaseNameResult + " " + shortMessage;
-              } else {
-                testCaseNameResult = ":green_apple: " + testCaseNameResult;
-              }
-              testCaseNameResult = "    " + testCaseNameResult;
-              testCaseNameResult += "\r\n";
-              OUTPUTSTR += testCaseNameResult;
-            }
-          });
-          FULLSTR += OUTPUTSTR + "\r\n";
+      //begin test case details
+      const testcases = content.elements[0].elements;
+      testcases.forEach(function (item) {
+        if (item.name == "testcase") {
+          var testCaseNameResult = item.attributes.name;
+          testCaseNameResult += getTestCaseResult(item);
+          if (testCaseNameResult.includes("skipped")) {
+            testCaseNameResult = ":pineapple: " + testCaseNameResult;
+          } else if (testCaseNameResult.includes("failure")) {
+            var errorMessage = item.elements[0].attributes.message;
+            var shortMessage = errorMessage.split(/\r?\n/)[0];
+            testCaseNameResult =
+              ":apple: " + testCaseNameResult + " " + shortMessage;
+          } else {
+            testCaseNameResult = ":green_apple: " + testCaseNameResult;
+          }
+          testCaseNameResult = "    " + testCaseNameResult;
+          testCaseNameResult += "\r\n";
+          OUTPUTSTR += testCaseNameResult;
         }
-      }
-    });
-    return FULLSTR;
+      });
+      testReport += OUTPUTSTR + "\r\n";
+    }
+    return testReport;
   }
 
   //function for interpret xml test case:
