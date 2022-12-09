0b6b7668eda63bc8a075ec3265012945029fa18a
jyotishka7139 <jyotishka.dasgupta@byjus.com>
2022-12-09 15:30:55 +0530

    added slack func

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ try {
   const testDir = rootDir + outputDir;
   console.log("test directory: " + testDir);
 
-  //   const slackToken = core.getInput("slackToken");
-  //   const slackChannelId = core.getInput("slackChannelId");
+  const slackToken = core.getInput("slackToken");
+  const slackChannelId = core.getInput("slackChannelId");
 
   //Slack web hook init:
-  //   const { WebClient } = require("@slack/web-api");
-  //   const web = new WebClient(slackToken);
+  const { WebClient } = require("@slack/web-api");
+  const web = new WebClient(slackToken);
 
   //file system reader init:
   var fs = require("fs");
@@ -33,26 +33,26 @@ try {
     console.log("Found files: ");
     console.log(data);
 
-    let FINALSTR = "Hello " + nameToGreet + "!" + "\r\n";
+    let FINALSTR = "Hello " + nameToGreet + "!" + "\r\n\r\n";
 
     //remove this later
-    FINALSTR += parseData(data);
-    console.log(`final j string is ${FINALSTR}`);
+    // FINALSTR += parseData(data);
+    // console.log(`final j string is ${FINALSTR}`);
     //remove
 
-    // let promise = new Promise(function (resolve, reject) {
-    //   FINALSTR += parseData(data);
-    //   resolve(FINALSTR);
-    // });
+    let promise = new Promise(function (resolve, reject) {
+      FINALSTR += parseData(data);
+      resolve(FINALSTR);
+    });
 
-    // promise.then(
-    //   function (result) {
-    //     slackBot(FINALSTR);
-    //   },
-    //   function (error) {
-    //     slackBot("Parse failed");
-    //   }
-    // );
+    promise.then(
+      function (result) {
+        slackBot(FINALSTR);
+      },
+      function (error) {
+        slackBot("Parse failed");
+      }
+    );
   });
 
   //function for parse every XML:
@@ -67,10 +67,8 @@ try {
       var options = { ignoreComment: true, alwaysChildren: true };
       var content = convert.xml2js(xml, options); // or convert.xml2json(xml, options)
       const testcases = content.elements[0].elements;
-      //process and output attributes:
-      //if (short output stirng for passed test) else (long output string for failed test):
-      console.log(util.inspect(content, false, null, true /* enable colors */));
 
+      console.log(util.inspect(content, false, null, true /* enable colors */));
       console.log("calculating test results, deciding if abbreviate output");
       if (
         parseInt(content.elements[0].attributes.failures) == 0 &&
@@ -163,36 +161,26 @@ try {
     return testReport;
   }
 
-  //function for interpret xml test case:
-  function getTestCaseResult(inXML) {
-    var outStr = "";
-    inXML.elements.forEach(function (result) {
-      outStr += " ";
-      outStr += result.name;
-    });
-    return outStr;
-  }
-
   //function for send text to slack:
-  //   function slackBot(inString) {
-  //     console.log("Sending to Slack: ");
-  //     console.log(
-  //       "==============================================================="
-  //     );
-  //     console.log(inString);
-  //     console.log(
-  //       "==============================================================="
-  //     );
-  //     (async () => {
-  //       // See: https://api.slack.com/methods/chat.postMessage
-  //       const res = await web.chat.postMessage({
-  //         channel: slackChannelId,
-  //         text: inString,
-  //       });
-  //       // `res` contains information about the posted message
-  //       return "Message sent: ", res.ts;
-  //     })();
-  //   }
+  function slackBot(inString) {
+    console.log("Sending to Slack: ");
+    console.log(
+      "==============================================================="
+    );
+    console.log(inString);
+    console.log(
+      "==============================================================="
+    );
+    (async () => {
+      // See: https://api.slack.com/methods/chat.postMessage
+      const res = await web.chat.postMessage({
+        channel: slackChannelId,
+        text: inString,
+      });
+      // `res` contains information about the posted message
+      return "Message sent: ", res.ts;
+    })();
+  }
 } catch (error) {
   core.setFailed("setFailed: " + error.message);
   console.log(error.message);
